2d3ae00a9b2abefe5e29748be97364dba6f4d7b3
Shivam <shivam16sept97@gmail.com>
2020-10-05 08:20:21 +0530

    added js and jquery to restauranthome

--- js/restauranthome.js
+++ js/restauranthome.js
@@ -10,6 +10,40 @@ function removebtn(j){
 
 // remove the notification after dish is prepared
 function removenotif(j){
-    var x=document.querySelector(".notif"+ j);
-    x.classList.add("removevis");
+    $(".notif"+j).slideUp();
 }
+
+
+//Show different color when notification has not been viewed
+$(".notifs").hover(function(){
+    $(this).css("backgroundColor","#9BFA86");
+});
+
+
+//Initially only show 2 orders
+index=2;
+var x=$(".container > div");
+x.splice(0,2);
+$.each(x,function(){
+    $(this).hide();
+});
+
+
+//Clicking on view more shows 2 more orders
+$(".viewmore").click(function(){
+    
+    var x=$(".container > div");
+    len=x.length;
+    x.splice(0,index);
+    var cnt=0;
+    $.each(x,function(){
+        $(this).slideDown();
+        cnt++;
+        if(cnt==2)
+            return false;
+    });
+    index+=2;
+    if(index>=len){
+        $(".viewmore").hide();
+    }
+});
